9e597f9e7f62fa1405659ac25e9ebef685dd9a14
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-05-25 16:11:19 +0200

    Made some SEO improvements

--- dist/images/year.js
+++
@@ -1,5 +0,0 @@
-var copyrightYear = (function () {
-  var thisYear = new Date();
-  var year = thisYear.getFullYear();
-  document.getElementById('copyright').innerHTML = 'Copyright | ColGro Pty Ltd. | ' + year;
-}());
